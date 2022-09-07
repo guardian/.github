8c452c86de6168aa0f9e3cd6f02d8796e4f19766
Jon Soul <jonathan.soul@guardian.co.uk>
2022-09-07 15:15:27 +0100

    Update PR batching documentation
    Include script for automating creation of workflows and reference to it in the docs.
    
    Add improvements section to document outstanding issues

---
+++ .github/workflows/scripts/pr-batching.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# A script to help with adding the necessary workflow files to enable "PR batching" for dependency updates on your
+# project(s). Intended to work with nori https://github.com/Financial-Times/nori or similar tools
+
+mkdir -p .github/workflows
+
+cat << EOF > .github/workflows/dep-updates_tracking-branch.yml
+name: Maintain dependency update batching branch
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  update-dependency-update-branch:
+    name: Keep tracking branch up to date with main
+    uses: guardian/.github/.github/workflows/pr-batching_tracking-branch.yml@v1.0.1
+EOF
+
+cat << EOF > .github/workflows/dep-updates_set-automerge.yml
+name: Set automerge on dependency update PRs
+
+on:
+  pull_request:
+    branches:
+      - dependency-updates
+
+jobs:
+  set-automerge:
+    name: Set automerge on opened PRs targeting the tracking branch
+    permissions:
+      contents: write
+    uses: guardian/.github/.github/workflows/pr-batching_set-automerge.yml@v1.0.1
+EOF
+
+cat << EOF > .github/workflows/dep-updates_pr-tracking-branch-to-default.yml
+name: Create batch dependency update PR
+
+on:
+  schedule:
+    - cron: "35 10 1 * *"
+  # Provide support for manually triggering the workflow via GitHub.
+  workflow_dispatch:
+
+jobs:
+  pr-tracking-branch:
+    name: Open a PR from dependency-updates targeting main
+    uses: guardian/.github/.github/workflows/pr-batching_pr-tracking-branch-to-default.yml@v1.0.1
+EOF
+
+git add .github/workflows/dep-updates_*.yml
+git commit -m 'chore: Add dependency update workflows'
